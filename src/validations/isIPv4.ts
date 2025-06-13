6602fb4cfe9a08618a496132a5de78041f7d5335
Samarth2190 <samarthruia@gmail.com>
2025-06-14 03:03:20 +0530

    feat: added new api refn isIPv4

---
+++ src/validations/isIPv4.ts
@@ -0,0 +1,9 @@
+export const isIPv4 = (str: string): boolean => {
+  const parts = str.split('.');
+  if (parts.length !== 4) return false;
+  
+  return parts.every(part => {
+    const num = parseInt(part, 10);
+    return !isNaN(num) && num >= 0 && num <= 255 && part === num.toString();
+  });
+};
