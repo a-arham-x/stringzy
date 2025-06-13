1204111ad0a798c0b12b3a7dfceb371779e08fbd
Samarth2190 <samarthruia@gmail.com>
2025-06-14 04:11:10 +0530

    added references for isIPv4

--- src/validations/index.ts
+++ src/validations/index.ts
@@ -3,13 +3,14 @@ export { isEmail } from './isEmail';
 export { isEmpty } from './isEmpty';
 export { isSlug } from './isSlug';
 export { isURL } from './isURL';
+export { isIPv4 } from './isIPv4';
 
-import { isIPv4 } from 'net';
 import { isDate } from './isDate';
 import { isEmail } from './isEmail';
 import { isEmpty } from './isEmpty';
 import { isSlug } from './isSlug';
 import { isURL } from './isURL';
+import { isIPv4 } from './isIPv4';
 
 export const validations = {
   isDate,
