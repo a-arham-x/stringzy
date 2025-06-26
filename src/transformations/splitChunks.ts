96a2d1f824a0f385ea3813fbe7d96fad08a2471c
a-arham-x <108538325+a-arham-x@users.noreply.github.com>
2025-06-26 15:23:42 +0500

    added jsdocs to splitChunks transformation

--- src/transformations/splitChunks.ts
+++ src/transformations/splitChunks.ts
@@ -1,4 +1,14 @@
+/**
+ * Creates chunks of string based on a given chunk size
+ * @param text - Input string
+ * @param Number - Size of the chunk. Must be a positive integer. Defaults to 1.
+ * @returns An array of all the chunks created from the string based on the specified chunk size.
+ * @throws Error if input text is not a string or chunk size is not a positive integer.
+ */
 export function splitChunks(text: string, chunkSize: Number = 1): string[] {
+    if (typeof text !== 'string') {
+        throw new Error('Input text must be a string');
+    } 
     if (
         typeof chunkSize !== 'number' ||
         !Number.isInteger(chunkSize) ||
